ed9d9fef131b3b2cf32936778c22ae7edafb6e68
Vinicius_VM <96557487+Vinicius-VM@users.noreply.github.com>
2024-03-12 11:47:48 -0300

    Add files via upload

--- pedir/armasBrancas/script1.js
+++ pedir/armasBrancas/script1.js
@@ -2,10 +2,10 @@ function enviar(){
     var mensagem = "Olá, sou seu cliente e resolvi fazer a aquisição do bastão tático. Me mande a chave pix para efeturar a compra.";
 
 
-    var custo = 102.00;
+    var custo = "São R$ 102.00 ao todo";
 
     var numeroTel = "5541997637207";
-    var linkWhatsapp = "https://wa.me/" + numeroTel + "?text= " + mensagem;
+    var linkWhatsapp = "https://wa.me/" + numeroTel + "?text= " + mensagem + " - " + custo;
     window.open(linkWhatsapp, "_blank");
 }
 
